17c6634bb39eaf2ae517f0d9e3aa1c442f92ba52
RodionKirillov <rodion.44454@gmail.com>
2024-08-28 10:43:52 +0300

    2.1

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
 
-
+            Greeting(name = "Android")
             UserInfo(userName = "Родион", userAge = 25)
 
 
