c21393ce980120c835242f58917e3c348ce33545
RodionKirillov <rodion.44454@gmail.com>
2024-08-28 15:35:37 +0300

    2.6

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -6,27 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.example.firstcomposeproject.ui.theme.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
@@ -34,40 +27,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            InstagramProfileCard()
+            FirstComposeProjectTheme {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                ) {
+                    InstagramProfileCard()
+                }
+            }
         }
     }
 }
 
-fun backgroundColor(column: Int, row: Int): Color {
-    return if ((column + row) % 2 == 0) {
-        Color.Yellow
-    } else {
-        Color.White
-    }
-}
-
-@Preview
-@Composable
-fun CardTest() {
-    Card(
-        shape = RoundedCornerShape(
-            topStart = 4.dp,
-            topEnd = 4.dp,
-        ),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.Green,
-            contentColor = Color.White
-        ),
-        border = BorderStroke(
-            1.dp,
-            Color.Black
-        )
-    ) {
-        Text(
-            modifier = Modifier.padding(8.dp),
-            text = "Hello World!"
-        )
-    }
-}
 
