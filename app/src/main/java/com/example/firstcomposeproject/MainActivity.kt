ccb09053654144e0ea7e73acba26f6044d6822d0
RodionKirillov <rodion.44454@gmail.com>
2024-08-28 11:55:23 +0300

    2.3

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -4,40 +4,68 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.firstcomposeproject.ui.theme.FirstComposeProjectTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
-            UserInfo(userName = "Родион", userAge = 25)
-
-
+            TimesTable()
         }
+    }
+}
+
+fun backgroundColor(column: Int, row: Int): Color {
+    return if ((column + row) % 2 == 0) {
+        Color.Yellow
+    } else {
+        Color.White
     }
 }
 
 @Preview
 @Composable
-fun Greeting() {
-    UserInfo(userName = "Родион", userAge = 25)
-}
-
-@Composable
-fun UserInfo(userName: String, userAge: Int) {
-    Column {
-        repeat(10) {
-            Text(text = "Привет $userName, тебе $userAge лет")
+fun TimesTable() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        for (i in 1 until 10) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                for (j in 1 until 10) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .background(color = backgroundColor(column = i, row = j))
+                            .border(width = 1.dp, color = Color.Black),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "${i * j}", color = Color.Black)
+                    }
+                }
+            }
         }
     }
 }
+
