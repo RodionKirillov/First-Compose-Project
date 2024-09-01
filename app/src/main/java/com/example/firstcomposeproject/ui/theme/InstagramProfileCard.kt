87e41dcecab8ca0a9644b0479620ad9dee6db0d5
RodionKirillov <rodion.44454@gmail.com>
2024-09-01 20:08:04 +0300

    4.2

--- app/src/main/java/com/example/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/example/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -4,10 +4,8 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,12 +13,19 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Recomposer
+import androidx.compose.runtime.State
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -32,11 +37,15 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.Lifecycle
+import com.example.firstcomposeproject.MainViewModel
 import com.example.firstcomposeproject.R
 
 @Composable
-fun InstagramProfileCard() {
-
+fun InstagramProfileCard(
+    viewModel: MainViewModel
+) {
+    val isFollowed: State<Boolean> = viewModel.isFollowing.observeAsState(false)
     Card(
         modifier = Modifier
             .padding(8.dp),
@@ -92,15 +101,37 @@ fun InstagramProfileCard() {
                 text = "https://www.instagram.com/facebook/",
                 fontSize = 16.sp,
             )
-            Button(
-                onClick = {},
-            ) {
-                Text(
-                    text = "Follow",
-                    fontWeight = FontWeight.Bold
-                )
+            FollowButton(isFollowed = isFollowed.value) {
+                viewModel.changeFollowingStatus()
             }
         }
+    }
+}
+
+@Composable
+private fun FollowButton(
+    isFollowed: Boolean,
+    clickListener: () -> Unit
+) {
+    Button(
+        onClick = { clickListener() },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (isFollowed) {
+                MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+            } else {
+                MaterialTheme.colorScheme.primary
+            }
+        )
+    ) {
+        val text = if (isFollowed) {
+            "Unfollow"
+        } else {
+            "Follow"
+        }
+        Text(
+            text = text,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
@@ -125,25 +156,5 @@ private fun UserStatistics(
         Text(
             text = title
         )
-    }
-}
-
-@Preview
-@Composable
-private fun PreviewCardLight() {
-    FirstComposeProjectTheme(
-        darkTheme = false
-    ) {
-        InstagramProfileCard()
-    }
-}
-
-@Preview
-@Composable
-private fun PreviewCardDark() {
-    FirstComposeProjectTheme(
-        darkTheme = true
-    ) {
-        InstagramProfileCard()
     }
 }
