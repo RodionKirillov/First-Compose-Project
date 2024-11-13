15f260882dd1533dc80ca7a223155a54c5444924
RodionKirillov <rodion.44454@gmail.com>
2024-11-13 18:27:19 +0300

    _2.6

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,6 +18,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.example.firstcomposeproject.ui.theme.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +26,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            InstagramProfileCard()
+            FirstComposeProjectTheme {
+                Scaffold {
+                    Box(
+                        modifier = Modifier
+                            .padding(it)
+                    ) {
+                        InstagramProfileCard()
+                    }
+                }
+            }
         }
     }
 }
@@ -49,4 +61,3 @@ private fun CardTest() {
         )
     }
 }
-
--- app/src/main/java/com/example/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/example/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,12 +21,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-@Preview
 @Composable
 fun InstagramProfileCard() {
     Card(
+        modifier = Modifier
+            .padding(8.dp),
         colors = CardDefaults.cardColors(
-            contentColor = Color.White,
+            contentColor = MaterialTheme.colorScheme.background,
         ),
         shape = RoundedCornerShape(
             topStart = 4.dp,
@@ -33,14 +35,13 @@ fun InstagramProfileCard() {
         ),
         border = BorderStroke(
             width = 1.dp,
-            color = Color.Black
+            color = MaterialTheme.colorScheme.onBackground
         )
 
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(Color.Gray)
                 .padding(8.dp),
             horizontalArrangement = Arrangement.SpaceEvenly,
             verticalAlignment = Alignment.CenterVertically
@@ -82,5 +83,25 @@ private fun TwoBoxes() {
         ) {
 
         }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardLight() {
+    FirstComposeProjectTheme(
+        darkTheme = false
+    ) {
+        InstagramProfileCard()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardDark() {
+    FirstComposeProjectTheme(
+        darkTheme = true
+    ) {
+        InstagramProfileCard()
     }
 }
