967619ed2b462788aa9675e3843817a12f63f0f9
RodionKirillov <rodion.44454@gmail.com>
2024-11-13 14:00:29 +0300

    _2.2

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -4,22 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.dp
-import java.nio.file.WatchEvent.Modifier
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Column() {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = Color.Cyan)
+                    .statusBarsPadding()
+                    .navigationBarsPadding()
+            ) {
                 UsrInfo(userName = "Rodion", userAge = 25)
-                Greeting(name = "Android")
+                UserInfoPreview()
             }
         }
     }
@@ -30,9 +41,12 @@ fun UsrInfo(userName: String, userAge: Int) {
     Text(text = "Привет $userName, Тебе $userAge")
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(
-        text = "Hello $name!",
-    )
+fun UserInfoPreview() {
+    Column {
+        repeat(10) {
+            UsrInfo(userName = "Rodion", userAge = 25)
+        }
+    }
 }
